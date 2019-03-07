8ec462a721d7f1ac4dd1caa982b3a1c0520af314
Phenomenon <1149650442@qq.com>
2019-03-07 21:32:34 +0800

    c7(ver 1.0)

---
+++ c7/c7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct William {
+	string name;
+	double size;
+	double weight;
+};
+
+int main()
+{
+	string name0;
+	double size0, weight0;
+	cout << "Enter the name: ";
+	getline(cin, name0);
+	cout << "Enter the size: ";
+	cin >> size0;
+	cout << "Enter the weight: ";
+	cin >> weight0;
+	William userDefine{
+		name0,
+		size0,
+		weight0
+	};
+	cout << "the name is " << userDefine.name << endl
+		<< "the size is " << userDefine.size << endl
+		<< "the weight is " << userDefine.weight;
+}
